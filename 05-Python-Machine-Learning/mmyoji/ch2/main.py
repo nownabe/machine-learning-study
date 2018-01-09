191cbcb971483acdd6f44d154e83cc00ff8fb493
Masaya Myojin <mmyoji@gmail.com>
2018-01-09 20:31:36 +0900

    Show Perceptron result

--- 05-Python-Machine-Learning/mmyoji/ch2/main.py
+++ 05-Python-Machine-Learning/mmyoji/ch2/main.py
@@ -17,10 +17,21 @@ y = np.where(y == 'Iris-setosa', -1, 1)
 # Extract 1 and 3 columns from 1~100 rows
 X = df.iloc[0:100, [0, 2]].values
 
-plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
-plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
+# plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
+# plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
+#
+# plt.xlabel('sepal length [cm]')
+# plt.ylabel('petal length [cm]')
+# plt.legend(loc='uppper left')
+# plt.show()
 
-plt.xlabel('sepal length [cm]')
-plt.ylabel('petal length [cm]')
-plt.legend(loc='uppper left')
+import perceptron
+
+ppn = perceptron.Perceptron(eta=0.1, n_iter=10)
+ppn.fit(X, y)
+
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('# of misclassifications')
 plt.show()
+
