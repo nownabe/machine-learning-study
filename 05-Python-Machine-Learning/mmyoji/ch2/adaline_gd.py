e0193e880cbf27b7a4f048b829653da1eeea5f07
Masaya Myojin <mmyoji@gmail.com>
2018-01-10 13:16:48 +0900

    Add ADELINE class

---
+++ 05-Python-Machine-Learning/mmyoji/ch2/adaline_gd.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+class AdalineGD(object):
+    """ADAptive LInear NEuron classifier
+
+    Params
+    ------
+    eta : float
+    n_iter : int
+
+    """
+
+    def __init__(self, eta=0.01, n_iter=50):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    def fit(self, X, y):
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            # Calculate activation function output
+            output = self.net_input(X)
+
+            errors = (y - output)
+
+            self.w_[1] += self.eta * X.T.dot(errors)
+
+            self.w_[0] += self.eta * errors.sum()
+
+            cost = (errors ** 2).sum() / 2.0
+
+            self.cost_.append(cost)
+
+        return self
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        return self.net_input(X)
+
+    def predict(self, X):
+        return np.where(self.activation(X) >= 0.0, 1, -1)
