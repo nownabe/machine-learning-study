74d479fc70a3f745162d2bef070347f7e364871b
Masaya Myojin <mmyoji@gmail.com>
2018-01-10 13:39:11 +0900

    ADALINE decision region

--- 05-Python-Machine-Learning/mmyoji/ch2/main.py
+++ 05-Python-Machine-Learning/mmyoji/ch2/main.py
@@ -46,17 +46,37 @@ from my_functions import plot_decision_regions
 
 from adaline_gd import AdalineGD
 
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
-ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
-ax[0].plot(range(1, len(ada1.cost_)+1), np.log10(ada1.cost_), marker='o')
-ax[0].set_xlabel('Epochs')
-ax[0].set_ylabel('log(Sum-squared-error)')
-ax[0].set_title('Adaline - Learning rate 0.01')
+# fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+# ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
+# ax[0].plot(range(1, len(ada1.cost_)+1), np.log10(ada1.cost_), marker='o')
+# ax[0].set_xlabel('Epochs')
+# ax[0].set_ylabel('log(Sum-squared-error)')
+# ax[0].set_title('Adaline - Learning rate 0.01')
 
-ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
-ax[1].plot(range(1, len(ada2.cost_)+1), ada2.cost_, marker='o')
-ax[1].set_xlabel('Epochs')
-ax[1].set_ylabel('Sum-squared-error')
-ax[1].set_title('Adaline - Learning rate 0.0001')
+# ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
+# ax[1].plot(range(1, len(ada2.cost_)+1), ada2.cost_, marker='o')
+# ax[1].set_xlabel('Epochs')
+# ax[1].set_ylabel('Sum-squared-error')
+# ax[1].set_title('Adaline - Learning rate 0.0001')
 
+# plt.show()
+
+X_std = np.copy(X)
+
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+ada = AdalineGD(n_iter=15, eta=0.01)
+ada.fit(X_std, y)
+
+plot_decision_regions(X_std, y, classifier=ada)
+plt.title('Adaline - Gradient Descent')
+plt.xlabel('sepal length [standarized')
+plt.ylabel('petal length [standarized')
+plt.legend(loc='upper left')
+plt.show()
+
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Sum-squared-error')
 plt.show()
