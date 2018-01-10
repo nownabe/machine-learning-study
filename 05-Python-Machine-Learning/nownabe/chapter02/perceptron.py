5a248f044c6620c4695c8dedc44854251c7dcd98
nownabe <nownabe@gmail.com>
2018-01-10 14:45:21 +0900

    2.6 大規模な機械学習と確率的勾配降下法

--- 05-Python-Machine-Learning/nownabe/chapter02/perceptron.py
+++ 05-Python-Machine-Learning/nownabe/chapter02/perceptron.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+from numpy.random import seed
 
 class Perceptron(object):
     def __init__(self, eta=0.01, n_iter=10):
@@ -71,3 +72,63 @@ class AdalineGD(object):
 
     def predict(self, X):
         return np.where(self.activation(X) >= 0.0, 1, -1)
+
+class AdalineSGD(object):
+    def __init__(self, eta=0.01, n_iter=50, shuffle=True, random_state=None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized= False
+        self.shuffle = shuffle
+        if random_state:
+            seed(random_state)
+
+    def fit(self, X, y):
+        self._initialize_weights(X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            cost = []
+            for xi, target in zip(X, y):
+                cost.append(self._update_weights(xi, target))
+            self.cost_.append(sum(cost) / len(y))
+
+        return self
+
+    def partial_fit(self, X, y):
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+
+        return self
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        return self.net_input(X)
+
+    def predict(self, X):
+        return np.where(self.activation(X) >= 0.0, 1, -1)
+
+    def _shuffle(self, X, y):
+        r = np.random.permutation(len(y))
+        return X[r], y[r]
+
+    def _initialize_weights(self, m):
+        self.w_ = np.zeros(1 + m)
+        self.w_initialized = True
+
+    def _update_weights(self, xi, target):
+        output = self.net_input(xi)
+        error = (target - output)
+        self.w_[1:] += self.eta * xi.dot(error)
+        self.w_[0] += self.eta * error
+        cost = 0.5 * error ** 2
+        return cost
--- 05-Python-Machine-Learning/nownabe/chapter02/test.py
+++ 05-Python-Machine-Learning/nownabe/chapter02/test.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-from perceptron import Perceptron, plot_decision_regions, AdalineGD
+from perceptron import Perceptron, plot_decision_regions, AdalineGD, AdalineSGD
 
 IRIS_URL = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 
@@ -64,9 +64,24 @@ X_std = np.copy(X)
 X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
 X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
 
-ada = AdalineGD(n_iter=10, eta=0.01).fit(X_std, y)
+# ada = AdalineGD(n_iter=10, eta=0.01).fit(X_std, y)
+# plot_decision_regions(X_std, y, classifier=ada)
+# plt.title("Adaline - Gradient Descent")
+# plt.xlabel("sepal length [standardized]")
+# plt.ylabel("petal length standardized[]")
+# plt.legend(loc="upper left")
+# plt.show()
+#
+# plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker="o")
+# plt.xlabel("Epochs")
+# plt.ylabel("Sum-squared-error")
+# plt.show()
+
+# --- 2.6
+ada = AdalineSGD(n_iter=15, eta=0.01, random_state=1)
+ada.fit(X_std, y)
 plot_decision_regions(X_std, y, classifier=ada)
-plt.title("Adaline - Gradient Descent")
+plt.title("Adaline - Stochastic Gradient Descent")
 plt.xlabel("sepal length [standardized]")
 plt.ylabel("petal length standardized[]")
 plt.legend(loc="upper left")
@@ -74,5 +89,5 @@ plt.show()
 
 plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker="o")
 plt.xlabel("Epochs")
-plt.ylabel("Sum-squared-error")
+plt.ylabel("Average Cost")
 plt.show()
