71400cdd9722215c2b8326f94d3c90677d3caea7
mmyoji <mmyoji@gmail.com>
2018-01-15 18:57:50 +0900

    Standarize training data with StandardScaler

--- 05-Python-Machine-Learning/mmyoji/ch3/main.py
+++ 05-Python-Machine-Learning/mmyoji/ch3/main.py
@@ -6,4 +6,17 @@ iris = datasets.load_iris()
 X = iris.data[:, [2, 3]]
 y = iris.target
 
-print("Class labels:", np.unique(y))
+# print("Class labels:", np.unique(y))
+
+from sklearn.model_selection import train_test_split
+
+# 30% test data, 70% train data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+sc.fit(X_train)
+
+X_train_std = sc.transform(X_train)
+X_test_std  = sc.transform(X_test)
