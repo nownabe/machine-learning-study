1afa879f87c1a6f18bccd9eff8fb7f0ffd53f3fb
mmyoji <mmyoji@gmail.com>
2018-01-16 19:49:21 +0900

    Finish 3.3

--- 05-Python-Machine-Learning/mmyoji/ch3/main.py
+++ 05-Python-Machine-Learning/mmyoji/ch3/main.py
@@ -37,8 +37,8 @@ from sklearn.metrics import accuracy_score
 from my_funcs import plot_decision_regions
 import matplotlib.pyplot as plt
 
-# X_combined_std = np.vstack((X_train_std, X_test_std))
-# y_combined = np.hstack((y_train, y_test))
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
 # plot_decision_regions(X=X_combined_std, y=y_combined, classifier=ppn,
 #                       test_idx=range(105, 150))
 # plt.xlabel('petal length [standarized]')
@@ -49,14 +49,43 @@ import matplotlib.pyplot as plt
 def sigmoid(z):
     return 1.0 / (1.0 + np.exp(-z))
 
-z = np.arange(-7, 7, 0.1)
-phi_z = sigmoid(z)
-plt.plot(z, phi_z)
-plt.axvline(0.0, color='k')
-plt.ylim(-0.1, 1.1)
-plt.xlabel('z')
-plt.ylabel('$\phi (z)$')
-plt.yticks([0.0, 0.5, 1.0])
-ax = plt.gca()
-ax.yaxis.grid(True)
+# z = np.arange(-7, 7, 0.1)
+# phi_z = sigmoid(z)
+# plt.plot(z, phi_z)
+# plt.axvline(0.0, color='k')
+# plt.ylim(-0.1, 1.1)
+# plt.xlabel('z')
+# plt.ylabel('$\phi (z)$')
+# plt.yticks([0.0, 0.5, 1.0])
+# ax = plt.gca()
+# ax.yaxis.grid(True)
+# plt.show()
+
+from sklearn.linear_model import LogisticRegression
+
+# lr = LogisticRegression(C=1000.0, random_state=0)
+# lr.fit(X_train_std, y_train)
+# plot_decision_regions(X_combined_std, y_combined, classifier=lr, test_idx=range(105, 150))
+# plt.xlabel('petal length [standarized]')
+# plt.ylabel('petal width [standarized]')
+# plt.legend(loc='upper left')
+# plt.show()
+
+# lr.predict_proba(X_test_std[0, :])
+
+weights, params = [], []
+
+for c in np.arange(-5, 5, dtype=float):
+    lr = LogisticRegression(C=10**c, random_state=0)
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10**c)
+
+weights = np.array(weights)
+plt.plot(params, weights[:, 0], label='petal length')
+plt.plot(params, weights[:, 1], label='petal width', linestyle="--")
+plt.ylabel('weight conefficient')
+plt.xlabel('C')
+plt.legend(loc='upper left')
+plt.xscale('log')
 plt.show()
