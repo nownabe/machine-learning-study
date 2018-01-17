d258641ecf63b1009e737b874148588c0aba81b8
mmyoji <mmyoji@gmail.com>
2018-01-17 19:35:18 +0900

    Finish 3.6

--- 05-Python-Machine-Learning/mmyoji/ch3/main.py
+++ 05-Python-Machine-Learning/mmyoji/ch3/main.py
@@ -131,10 +131,61 @@ y_xor = np.where(y_xor, 1, -1)
 # plt.legend(loc='upper left')
 # plt.show()
 
-svm = SVC(kernel='rbf', random_state=0, gamma=100.0, C=1.0)
-svm.fit(X_train_std, y_train)
-plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
-plt.xlabel('petal length [standarized]')
-plt.ylabel('petal width  [standarized]')
+# svm = SVC(kernel='rbf', random_state=0, gamma=100.0, C=1.0)
+# svm.fit(X_train_std, y_train)
+# plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
+# plt.xlabel('petal length [standarized]')
+# plt.ylabel('petal width  [standarized]')
+# plt.legend(loc='upper left')
+# plt.show()
+
+# def gini(p):
+#     return p*(1-p) + (1-p)*(1 - (1-p))
+# 
+# def entropy(p):
+#     return - p*np.log2(p) - (1-p) * np.log2(1 - p)
+# 
+# def error(p):
+#     return 1 - np.max([p, 1-p])
+# 
+# x = np.arange(0.0, 1.0, 0.01)
+# ent = [entropy(p) if p != 0 else None for p in x]
+# sc_ent = [e * 0.5 if e else None for e in ent]
+# err = [error(i) for i in x]
+# fig = plt.figure()
+# ax = plt.subplot(111)
+# for i, lab, ls, c, in zip([ent, sc_ent, gini(x), err],
+#                           ['Entropy', 'Entropy (scaled)', 'Gini Impurity', 'Misclassification error'],
+#                           ['-', '-', '--', '-.'],
+#                           ['black', 'lightgray', 'red', 'green', 'cyan']):
+#     line = ax.plot(x, i, label=lab, linestyle=ls, lw=2, color=c)
+# ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=3,
+#           fancybox=True, shadow=False)
+# ax.axhline(y=0.5, linewidth=1, color='k', linestyle='--')
+# ax.axhline(y=1.0, linewidth=1, color='k', linestyle='--')
+# plt.ylim([0, 1.1])
+# plt.xlabel('p(i=1)')
+# plt.ylabel('Impurity index')
+# plt.show()
+
+# from sklearn.tree import DecisionTreeClassifier
+# 
+# tree = DecisionTreeClassifier(criterion="entropy", max_depth=3, random_state=0)
+# tree.fit(X_train, y_train)
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+# plot_decision_regions(X_combined, y_combined, classifier=tree, test_idx=range(105, 150))
+# plt.xlabel('petal length [cm]')
+# plt.ylabel('petal width  [cm]')
+# plt.legend(loc='upper left')
+# plt.show()
+
+from sklearn.ensemble import RandomForestClassifier
+
+forest = RandomForestClassifier(criterion="entropy", n_estimators=10, random_state=1, n_jobs=2)
+forest.fit(X_train, y_train)
+plot_decision_regions(X_combined, y_combined, classifier=forest, test_idx=range(105, 150))
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width  [cm]')
 plt.legend(loc='upper left')
 plt.show()
