588e61a50fdd3777b865d8b150d39069cbaef2ae
mmyoji <mmyoji@gmail.com>
2018-01-17 19:50:20 +0900

    Finish 3.7

--- 05-Python-Machine-Learning/mmyoji/ch3/main.py
+++ 05-Python-Machine-Learning/mmyoji/ch3/main.py
@@ -180,12 +180,17 @@ y_combined = np.hstack((y_train, y_test))
 # plt.legend(loc='upper left')
 # plt.show()
 
-from sklearn.ensemble import RandomForestClassifier
+# from sklearn.ensemble import RandomForestClassifier
+# 
+# forest = RandomForestClassifier(criterion="entropy", n_estimators=10, random_state=1, n_jobs=2)
+# forest.fit(X_train, y_train)
+# plot_decision_regions(X_combined, y_combined, classifier=forest, test_idx=range(105, 150))
+# plt.xlabel('petal length [cm]')
+# plt.ylabel('petal width  [cm]')
+# plt.legend(loc='upper left')
+# plt.show()
 
-forest = RandomForestClassifier(criterion="entropy", n_estimators=10, random_state=1, n_jobs=2)
-forest.fit(X_train, y_train)
-plot_decision_regions(X_combined, y_combined, classifier=forest, test_idx=range(105, 150))
-plt.xlabel('petal length [cm]')
-plt.ylabel('petal width  [cm]')
-plt.legend(loc='upper left')
-plt.show()
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors=5, p=2, metric='minkowski')
+knn.fit(X_train_std, y_train)
