858f7dd796adf541c20381cae33daea6c2c655d9
mmyoji <mmyoji@gmail.com>
2018-01-22 20:37:41 +0900

    Finish 4.6

--- 05-Python-Machine-Learning/mmyoji/ch4/main.py
+++ 05-Python-Machine-Learning/mmyoji/ch4/main.py
@@ -8,30 +8,40 @@ df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
                    'Alcalinity of ash', 'Magnesium', 'Total phenols', 'Flavanoids',
                    'Nonflavanoid phenols', 'Proanthocyanins', 'Color intensity', 'Hue',
                    'OD280/OD315 of diluted wines', 'Proline']
-print('Class labels', np.unique(df_wine['Class label']))
-df_wine.head()
+# print('Class labels', np.unique(df_wine['Class label']))
+# df_wine.head()
 
 from sklearn.model_selection import train_test_split
 
 X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
-from sklearn.preprocessing import MinMaxScaler
+# from sklearn.preprocessing import MinMaxScaler
+# 
+# mms = MinMaxScaler()
+# X_train_norm = mms.fit_transform(X_train)
+# X_test_norm = mms.transform(X_test)
+# 
+# from sklearn.preprocessing import StandardScaler
+# 
+# stdsc = StandardScaler()
+# X_train_std = stdsc.fit_transform(X_train)
+# X_test_std = stdsc.transform(X_test)
+# 
+# from sklearn.neighbors import KNeighborsClassifier
+# import matplotlib.pyplot as plt
+# from sbs import SBS
+# 
+# knn = KNeighborsClassifier(n_neighbors=2)
+# sbs = SBS(knn, k_features=1)
+# sbs.fit(X_train_std, y_train)
 
-mms = MinMaxScaler()
-X_train_norm = mms.fit_transform(X_train)
-X_test_norm = mms.transform(X_test)
+from sklearn.ensemble import RandomForestClassifier
 
-from sklearn.preprocessing import StandardScaler
-
-stdsc = StandardScaler()
-X_train_std = stdsc.fit_transform(X_train)
-X_test_std = stdsc.transform(X_test)
-
-from sklearn.neighbors import KNeighborsClassifier
-import matplotlib.pyplot as plt
-from sbs import SBS
-
-knn = KNeighborsClassifier(n_neighbors=2)
-sbs = SBS(knn, k_features=1)
-sbs.fit(X_train_std, y_train)
+feat_labels = df_wine.columns[1:]
+forest = RandomForestClassifier(n_estimators=10000, random_state=0, n_jobs=-1)
+forest.fit(X_train, y_train)
+importances = forest.feature_importances_
+indices = np.argsort(importances)[::-1]
+for f in range(X_train.shape[1]):
+    print("%2d) %-*s %f" % (f+1, 30, feat_labels[indices[f]], importances[indices[f]]))
