23c05d4aa92093eee67346977b73a48a61911f22
mmyoji <mmyoji@gmail.com>
2018-01-22 19:31:20 +0900

    Finish 4.4

--- 05-Python-Machine-Learning/mmyoji/ch4/main.py
+++ 05-Python-Machine-Learning/mmyoji/ch4/main.py
@@ -15,3 +15,15 @@ from sklearn.model_selection import train_test_split
 
 X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+from sklearn.preprocessing import MinMaxScaler
+
+mms = MinMaxScaler()
+X_train_norm = mms.fit_transform(X_train)
+X_test_norm = mms.transform(X_test)
+
+from sklearn.preprocessing import StandardScaler
+
+stdsc = StandardScaler()
+X_train_std = stdsc.fit_transform(X_train)
+X_test_std = stdsc.transform(X_test)
