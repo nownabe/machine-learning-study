20f2a6b8cb49fa0cdc08a1a1783d8ab466adf415
mmyoji <mmyoji@gmail.com>
2018-01-22 19:29:22 +0900

    Finish 4.3

---
+++ 05-Python-Machine-Learning/mmyoji/ch4/main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+df_wine = pd.read_csv(
+    'https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data',
+    header=None)
+df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
+                   'Alcalinity of ash', 'Magnesium', 'Total phenols', 'Flavanoids',
+                   'Nonflavanoid phenols', 'Proanthocyanins', 'Color intensity', 'Hue',
+                   'OD280/OD315 of diluted wines', 'Proline']
+print('Class labels', np.unique(df_wine['Class label']))
+df_wine.head()
+
+from sklearn.model_selection import train_test_split
+
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
