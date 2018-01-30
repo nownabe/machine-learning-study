f401f9c582df8a0226cf3fd12c5066826bf5b44e
HirokiTerashima <hiroki@dev.local>
2018-01-31 00:01:03 +0900

    5.2

---
+++ 05-Python-Machine-Learning/tera/chapter05/5.2.py
@@ -0,0 +1,157 @@
+from distutils.version import LooseVersion as Version
+from sklearn import __version__ as sklearn_version
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+
+if Version(sklearn_version) < '0.18':
+    from sklearn.cross_validation import train_test_split
+else:
+    from sklearn.model_selection import train_test_split
+
+if Version(sklearn_version) < '0.18':
+    from sklearn.lda import LDA
+else:
+    from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+
+df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/'
+                      'machine-learning-databases/wine/wine.data',
+                      header=None)
+
+df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
+                   'Alcalinity of ash', 'Magnesium', 'Total phenols',
+                   'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins',
+                   'Color intensity', 'Hue',
+                   'OD280/OD315 of diluted wines', 'Proline']
+
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+X_test_std = sc.transform(X_test)
+
+np.set_printoptions(precision=4)
+
+mean_vecs = []
+for label in range(1, 4):
+    mean_vecs.append(np.mean(X_train_std[y_train == label], axis=0))
+    print('MV %s: %s\n' % (label, mean_vecs[label - 1]))
+
+
+d = 13
+S_W = np.zeros((d, d))
+for label, mv in zip(range(1, 4), mean_vecs):
+    class_scatter = np.zeros((d, d))
+    for row in X_train_std[y_train == label]:
+        row, mv = row.reshape(d, 1), mv.reshape(d, 1)
+        class_scatter += (row - mv).dot((row - mv).T)
+    S_W += class_scatter
+print('Within-class scatter matrix: %sx%s' % (S_W.shape[0], S_W.shape[1]))
+print('Class label distribution: %s' % np.bincount(y_train)[1:])
+
+d = 13
+S_W = np.zeros((d, d))
+for label, mv in zip(range(1, 4), mean_vecs):
+    class_scatter = np.cov(X_train_std[y_train == label].T)
+    S_W += class_scatter
+print('Scaled within-class scatter matrix: %sx%s' % (S_W.shape[0], S_W.shape[1]))
+
+mean_overall = np.mean(X_train_std, axis=0)
+d = 13
+S_B = np.zeros((d, d))
+for i, mean_vec in enumerate(mean_vecs):
+    n = X_train[y_train == i + 1, :].shape[0]
+    mean_vec = mean_vec.reshape(d, 1)
+    mean_overall = mean_overall.reshape(d, 1)
+    S_B += n * (mean_vec - mean_overall).dot((mean_vec - mean_overall).T)
+print('Between-class scatter matrix: %sx%s' % (S_B.shape[0], S_B.shape[1]))
+
+eigen_vals, eigen_vecs = np.linalg.eig(np.linalg.inv(S_W).dot(S_B))
+
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i])
+               for i in range(len(eigen_vals))]
+
+eigen_pairs = sorted(eigen_pairs, key=lambda k: k[0], reverse=True)
+
+print('Eigenvalues in decreasing order:\n')
+for eigen_val in eigen_pairs:
+    print(eigen_val[0])
+
+tot = sum(eigen_vals.real)
+discr = [(i / tot) for i in sorted(eigen_vals.real, reverse=True)]
+cum_discr = np.cumsum(discr)
+
+# plt.bar(range(1, 14), discr, alpha=0.5, align='center', label='individual "discriminability"')
+# plt.step(range(1, 14), cum_discr, where='mid', label='cumulative "discriminability"')
+# plt.ylabel('"discriminability" ratio')
+# plt.xlabel('Linear Discriminants')
+# plt.ylim([-0.1, 1.1])
+# plt.legend(loc='best')
+# plt.tight_layout()
+# plt.show()
+
+w = np.hstack((eigen_pairs[0][1][:, np.newaxis].real, eigen_pairs[1][1][:, np.newaxis].real))
+print('Matrix W:\n', w)
+
+X_train_lda = X_train_std.dot(w)
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+
+for l, c, m in zip(np.unique(y_train), colors, markers):
+    plt.scatter(X_train_lda[y_train == l, 0] * (-1),
+                X_train_lda[y_train == l, 1] * (-1),
+                c=c, label=l, marker=m)
+
+# plt.xlabel('LD 1')
+# plt.ylabel('LD 2')
+# plt.legend(loc='lower right')
+# plt.tight_layout()
+# plt.show()
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.6,
+                    c=cmap(idx),
+                    edgecolor='black',
+                    marker=markers[idx],
+                    label=cl)
+
+lda = LDA(n_components=2)
+X_train_lda = lda.fit_transform(X_train_std, y_train)
+
+lr = LogisticRegression()
+lr = lr.fit(X_train_lda, y_train)
+
+# plot_decision_regions(X_train_lda, y_train, classifier=lr)
+# plt.xlabel('LD 1')
+# plt.ylabel('LD 2')
+# plt.legend(loc='lower left')
+# plt.tight_layout()
+# plt.show()
+
+X_test_lda = lda.transform(X_test_std)
+
+# plot_decision_regions(X_test_lda, y_test, classifier=lr)
+# plt.xlabel('LD 1')
+# plt.ylabel('LD 2')
+# plt.legend(loc='lower left')
+# plt.tight_layout()
+# plt.show()
