2e3f2b02b24f0d0f4242c186e8c5f87529e52bda
mmyoji <mmyoji@gmail.com>
2018-01-31 18:08:32 +0900

    Finish Ch 5.3

--- 05-Python-Machine-Learning/mmyoji/ch5/main.py
+++ 05-Python-Machine-Learning/mmyoji/ch5/main.py
@@ -154,3 +154,17 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 # plt.legend(loc='lower left')
 # plt.show()
 
+
+from sklearn.datasets import make_moons, make_circles
+from matplotlib.ticker import FormatStrFormatter
+from sklearn.decomposition import KernelPCA
+
+X, y = make_moons(n_samples=100, random_state=123)
+scikit_kpca = KernelPCA(n_components=2, kernel='rbf', gamma=15)
+X_skernpca = scikit_kpca.fit_transform(X)
+
+plt.scatter(X_skernpca[y==0, 0], X_skernpca[y==0, 1], color='red', marker='^', alpha=0.5)
+plt.scatter(X_skernpca[y==1, 0], X_skernpca[y==1, 1], color='blue', marker='o', alpha=0.5)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.show()
