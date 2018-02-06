2ab1120625a0800ceb5d7fa99a0ab17908d6f57b
mmyoji <mmyoji@gmail.com>
2018-02-06 17:28:29 +0900

    Remove unnecessary HTML markup from the review text

--- 05-Python-Machine-Learning/mmyoji/ch8/main.py
+++ 05-Python-Machine-Learning/mmyoji/ch8/main.py
@@ -2,6 +2,7 @@ import pyprind
 import pandas as pd
 import os
 import numpy as np
+import re
 
 CSV_FILE = './movie_data.csv'
 
@@ -31,3 +32,16 @@ CSV_FILE = './movie_data.csv'
 df = pd.read_csv(CSV_FILE)
 # # Check CSV is whether correct or not
 # print(df.head(3))
+
+# Remove HTML markup in `text`
+def preprocessor(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
+    text = re.sub('[\W]+', ' ', text.lower()) + ''.join(emoticons).replace('-', '')
+    return text
+
+# # Test the function
+# print(preprocessor(df.loc[0, 'review'][-50:]))
+# print(preprocessor("</a>This :) is <br/>:( a test :-)!<br><br>"))
+
+df['review'] = df['review'].apply(preprocessor)
