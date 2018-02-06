c990388f605a14094a2d190071e9efd649f8d0ad
mmyoji <mmyoji@gmail.com>
2018-02-06 20:01:13 +0900

    Finish long-run Pipeline operation
    > Fitting 5 folds for each of 48 candidates, totalling 240 fits
    > [Parallel(n_jobs=-1)]: Done  42 tasks      | elapsed: 18.6min
    > [Parallel(n_jobs=-1)]: Done 192 tasks      | elapsed: 88.6min
    > [Parallel(n_jobs=-1)]: Done 240 out of 240 | elapsed: 116.2min finished
    > Best parameter set: {'clf__C': 10.0, 'clf__penalty': 'l2',
    > 'vect__ngram_range': (1, 1), 'vect__stop_words': None,
    > 'vect__tokenizer': <function tokenizer at 0x7fcb46b7ff28>}
    > CV Accuracy: 0.892
    > Test Accuracy: 0.899

--- 05-Python-Machine-Learning/mmyoji/ch8/main.py
+++ 05-Python-Machine-Learning/mmyoji/ch8/main.py
@@ -4,6 +4,17 @@ import os
 import numpy as np
 import re
 
+from nltk.stem.porter import PorterStemmer
+
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 CSV_FILE = './movie_data.csv'
 
 # # Convert aclImdb files into a CSV file
@@ -45,3 +56,43 @@ def preprocessor(text):
 # print(preprocessor("</a>This :) is <br/>:( a test :-)!<br><br>"))
 
 df['review'] = df['review'].apply(preprocessor)
+
+def tokenizer(text):
+    return text.split()
+
+porter = PorterStemmer()
+
+def tokenizer_porter(text):
+    return [porter.stem(word) for word in text.split()]
+
+stop = stopwords.words('english')
+
+X_train = df.loc[:25000, 'review'].values
+y_train = df.loc[:25000, 'sentiment'].values
+X_test = df.loc[25000:, 'review'].values
+y_test = df.loc[25000:, 'sentiment'].values
+
+tfidf = TfidfVectorizer(strip_accents=None, lowercase=False, preprocessor=None)
+param_grid = [{'vect__ngram_range': [(1, 1)],
+               'vect__stop_words': [stop, None],
+               'vect__tokenizer': [tokenizer, tokenizer_porter],
+               'clf__penalty': ['l1', 'l2'],
+               'clf__C': [1.0, 10.0, 100.0]},
+              {'vect__ngram_range': [(1, 1)],
+               'vect__stop_words': [stop, None],
+               'vect__tokenizer': [tokenizer, tokenizer_porter],
+               'vect__use_idf': [False],
+               'vect__norm': [None],
+               'clf__penalty': ['l1', 'l2'],
+               'clf__C': [1.0, 10.0, 100.0]}]
+lr_tfidf = Pipeline([('vect', tfidf),
+                     ('clf', LogisticRegression(random_state=0))])
+gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid, scoring='accuracy', cv=5, verbose=1, n_jobs=-1)
+gs_lr_tfidf.fit(X_train, y_train)
+
+print('Best parameter set: %s ' % gs_lr_tfidf.best_params_)
+print('CV Accuracy: %.3f' % gs_lr_tfidf.best_score_)
+
+clf = gs_lr_tfidf.best_estimator_
+print('Test Accuracy: %.3f' % clf.score(X_test, y_test))
+
